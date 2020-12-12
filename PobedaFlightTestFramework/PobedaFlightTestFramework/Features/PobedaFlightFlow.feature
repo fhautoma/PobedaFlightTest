5efe25f5a3f27fc107fcb4dcbba663bc706e0100
Felipe Palacio <fpalacio@netactica.com>
2020-12-12 00:08:53 -0500

    v1.0

---
+++ PobedaFlightTestFramework/PobedaFlightTestFramework/Features/PobedaFlightFlow.feature
@@ -0,0 +1,11 @@
+﻿Feature: PobedaFlightFlow
+	- Prueba de navegacion a la URL de aerolinea pobeda
+	- Prueba de Interacción con página de Búsqueda
+	- Prueba de Interacción con la Selección de Vuelos
+
+
+
+	Scenario: Acceso a url automatizada
+		Given Navegar al sitio web de la aerolínea Pobeda
+			And Esperar que cargue el sitio web
+		Then La pagina carga correctamente
---
+++ PobedaFlightTestFramework/PobedaFlightTestFramework/StepDefinitions/PobedaFlightFlowSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace PobedaFlightTestFramework.StepDefinitions
+{
+    [Binding]
+    public class PobedaFlightFlowSteps
+    {
+        [Given(@"Navegar al sitio web de la aerolínea Pobeda")]
+        public void GivenNavegarAlSitioWebDeLaAerolineaPobeda()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Esperar que cargue el sitio web")]
+        public void GivenEsperarQueCargueElSitioWeb()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"La pagina carga correctamente")]
+        public void ThenLaPaginaCargaCorrectamente()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
